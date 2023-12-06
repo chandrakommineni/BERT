30de7eb177edba05f5b4299826ef3c3c80c2c8db
Debajyoti Chakraborty <111325167+chakrabortyde@users.noreply.github.com>
2023-12-06 12:46:31 -0500

    Added notebooks and supporting files

---
+++ DataCollatorForMultipleChoice.py
@@ -0,0 +1,44 @@
+# 🤗 Transformers doesn’t have a data collator for multiple choice, so we’ll need to adapt the DataCollatorWithPadding 
+# to create a batch of examples. It’s more efficient to dynamically pad the sentences to the longest length in a batch 
+# during collation, instead of padding the whole dataset to the maximum length.
+# DataCollatorForMultipleChoice flattens all the model inputs, applies padding, and then unflattens the results:
+# https://huggingface.co/docs/transformers/tasks/multiple_choice#preprocess
+
+from dataclasses import dataclass
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase, PaddingStrategy
+from typing import Optional, Union
+import torch
+
+
+@dataclass
+class DataCollatorForMultipleChoice:
+    """
+    Data collator that will dynamically pad the inputs for multiple choice received.
+    """
+
+    tokenizer: PreTrainedTokenizerBase
+    padding: Union[bool, str, PaddingStrategy] = True
+    max_length: Optional[int] = None
+    pad_to_multiple_of: Optional[int] = None
+
+    def __call__(self, features):
+        label_name = "label" if "label" in features[0].keys() else "labels"
+        labels = [feature.pop(label_name) for feature in features]
+        batch_size = len(features)
+        num_choices = len(features[0]["input_ids"])
+        flattened_features = [
+            [{k: v[i] for k, v in feature.items()} for i in range(num_choices)] for feature in features
+        ]
+        flattened_features = sum(flattened_features, [])
+
+        batch = self.tokenizer.pad(
+            flattened_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
+        batch["labels"] = torch.tensor(labels, dtype=torch.int64)
+        return batch
